0f32eb71897cbc92ce1b2ec135173c2201ac0079
John Taylor <johtaylo@microsoft.com>
2018-03-05 17:51:19 -0800

    initial work on port from javascript

---
+++ .NET/Microsoft.Recognizers.DataTypes.DateTime/Timex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Recognizers.DataTypes.DateTime
+{
+    public class Timex
+    {
+        public Timex(string timex)
+        {
+        }
+        public bool Now { get; set; }
+
+        public int Years { get; set }
+
+        public int Months { get; set }
+
+        public int Weeks { get; set }
+
+        public int Days { get; set }
+    }
+}
---
+++ .NET/Microsoft.Recognizers.DataTypes.DateTime/TimexParsing.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Recognizers.DataTypes.DateTime
+{
+    public static class TimexParsing
+    {
+        public static void ParseString(string timex, Timex obj)
+        {
+            // a reference to the present
+            if (timex == "PRESENT_REF")
+            {
+                obj.Now = true;
+            }
+            // duration
+            else if (timex.StartsWith("P"))
+            {
+                ExtractDuration(timex, obj);
+            }
+            // range indicated with start and end dates and a duration
+            else if (timex.StartsWith("(") && timex.EndsWith(")"))
+            {
+                ExtractStartEndRange(timex, obj);
+            }
+            // date and time and their respective ranges
+            else
+            {
+                ExtractDateTime(timex, obj);
+            }
+        }
+
+        private static void ExtractDuration(string s, Timex obj)
+        {
+            var extracted = new Dictionary<string, string>();
+            TimexRegex.Extract("period", s, extracted);
+            if (extracted.ContainsKey("dateUnit"))
+            {
+                ExtractDateUnit(extracted, obj);
+            }
+            else if (extracted.ContainsKey("timeUnit"))
+            {
+                ExtractTimeUnit(extracted, obj);
+            }
+        }
+
+        private static void ExtractDateUnit(IDictionary<string, string> extracted, Timex obj)
+        {
+            switch (extracted["dateUnit"])
+            {
+                case "Y":
+                    obj.Years = int.Parse(extracted["amount"]);
+                    break;
+                case "M":
+                    obj.Months = int.Parse(extracted["amount"]);
+                    break;
+                case "W":
+                    obj.Weeks = int.Parse(extracted["amount"]);
+                    break;
+                case "D":
+                    obj.Days = int.Parse(extracted["amount"]);
+                    break;
+            }
+        }
+
+        private static void ExtractTimeUnit(IDictionary<string, string> extracted, Timex obj)
+        {
+        }
+
+        private static void ExtractStartEndRange(string timex, Timex obj)
+        {
+        }
+
+        private static void ExtractDateTime(string timex, Timex obj)
+        {
+        }
+    }
+}
